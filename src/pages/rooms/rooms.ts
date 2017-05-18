117d0f4e2caef65e5348b1ea978178aa48621d2f
magnus-hansson <magnus.hansson@fritidsresor.se>
2017-05-18 12:59:10 +0200

    disabla Hammer.assign i vis.js

--- src/pages/rooms/rooms.ts
+++ src/pages/rooms/rooms.ts
@@ -13,7 +13,25 @@ export class RoomsPage {
   timeline:any;
   container: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    var items = new vis.DataSet([
+    
+  }
+
+  ionViewDidLoad() {
+  //   var items = new vis.DataSet([
+  //   {id: 1, content: 'item 1', start: '2013-04-20'},
+  //   {id: 2, content: 'item 2', start: '2013-04-14'},
+  //   {id: 3, content: 'item 3', start: '2013-04-18'},
+  //   {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
+  //   {id: 5, content: 'item 5', start: '2013-04-25'},
+  //   {id: 6, content: 'item 6', start: '2013-04-27'}
+  // ]);
+  //   this.container = document.getElementById('visualization');
+  //   this.timeline = new vis.Timeline(this.container,items);
+
+  var container = document.getElementById('visualization');
+
+  // Create a DataSet (allows two way data-binding)
+  var items = new vis.DataSet([
     {id: 1, content: 'item 1', start: '2013-04-20'},
     {id: 2, content: 'item 2', start: '2013-04-14'},
     {id: 3, content: 'item 3', start: '2013-04-18'},
@@ -21,11 +39,12 @@ export class RoomsPage {
     {id: 5, content: 'item 5', start: '2013-04-25'},
     {id: 6, content: 'item 6', start: '2013-04-27'}
   ]);
-    this.container = document.getElementById('visualization');
-    this.timeline = new vis.Timeline(this.container,items);
-  }
 
-  ionViewDidLoad() {
+  // Configuration for the Timeline
+  var options = {};
+
+  // Create a Timeline
+  this.timeline = new vis.Timeline(container, items, options);
     console.log('ionViewDidLoad RoomsPage');
   }
 
